35fe3411a7e2d25c26abf4df58da2f1c45e9c4ce
Hawkins <2398430768@qq.com>
2023-06-20 18:48:31 +0800

    secCommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SchoolTest></SchoolTest>
+    <StudentTest></StudentTest>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SchoolTest from './components/SchoolTest.vue'
+import StudentTest from './components/StudentTest.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { SchoolTest, StudentTest },
+  name:'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
