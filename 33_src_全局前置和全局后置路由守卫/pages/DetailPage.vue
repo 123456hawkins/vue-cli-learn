b0e4398739e17a814e6e94dbe1389a389b8d2590
Hawkins <2398430768@qq.com>
2023-06-30 15:08:11 +0800

    新增全局前置和全局后置路由

---
+++ 33_src_全局前置和全局后置路由守卫/pages/DetailPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li>消息编号:{{ id }}</li>
+    
+    <li>消息标题:{{ title }}</li>
+    <!-- <h1>{{ a }},{{ b }}</h1> -->
+  </ul>
+</template>
+
+<script>
+export default {
+  name:'DetailPage',
+  // props:['a','b'],
+  props:['id','title'],
+  // computed:{
+  //   id(){
+  //     return this.$route.params.id
+  //   },
+  //   title(){
+  //     return this.$route.params.title
+  //   }
+  // },
+  mounted(){
+    console.log('detail挂载');
+  }
+}
+</script>
+
+<style>
+
+</style>
